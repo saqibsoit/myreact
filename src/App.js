7151e4d0a04c7a506c258ae4834a70925059a6ce
saqibsoit <51706503+saqibsoit@users.noreply.github.com>
2021-09-07 18:55:23 +0500

    Add some CSS and Declare variable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import React, {useState} from 'react';
+import Message from './Message';
 import './App.css';
-import Dinner from './dinner.js';
+//import Dinner from './dinner.js';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let[isMorning, setMorning] =  useState(true);
   return (
-    <div className = 'App'>
-      <Dinner dishName = 'Chicken'/>
+    <div className = {`box ${isMorning ? 'dayLight' : 'box'}`}>
+      <h1>Good {isMorning ? 'Morning' : 'Night'} </h1>
+     <Message counter = {count}/>
+     <button onClick= {()=> setCount(++ count)} > Update Counter
+        </button>
+
+        <button onClick= {()=> setCount(0)} > Reset Counter
+        </button>
+
+        <button onClick= {()=> setMorning(!isMorning)} > Reset Day
+        </button>
     </div>
   );
 }
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Message(props){
+    return(
+        <h3>Value of Counter Variable is {props.counter}</h3>
+    )
+}
